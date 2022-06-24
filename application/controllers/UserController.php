f2c8e6c78300ad171a18ebdc9883b0603a06d9fe
HYE0139 <h.ye011096@gmail.com>
2022-06-24 18:11:27 +0900

    mvc0624

--- application/controllers/UserController.php
+++ application/controllers/UserController.php
@@ -1,15 +1,37 @@
 <?php
 namespace application\controllers;
 
-require_once "application/utils/UrlUtils.php";
 
 class UserController extends Controller {
-    public function signin() {        
-        return "user/signin.php";
+    public function signin() {
+        switch(getMethod()) {
+            case _GET :
+                return "user/signin.php";
+            case _POST : 
+                $email = $_POST["email"];
+                $param = ["email" => $email, "pw" => $_POST["pw"]];
+                $dbUser = $this-> model-> selUser($param);
+
+            if(!$dbUser || !password_verify($param["pw"], $dbUSer->pw)) {
+                return "redirect:signin?email={$email}&err";
+            } 
+                return "redirect:/feed/index";
+        }
     }
 
     public function signup() {
-        return "user/signup.php";
-        
+        switch(getMethod()) {
+            case _GET : // 로그인
+                return "user/signup.php";
+            case _POST : //회원가입
+                $param = [
+                    "email" => $_POST["email"],
+                    "pw" => $_POST["pw"],
+                    "nm" => $_POST["nm"],
+                ];
+                $param["pw"] = password_hash($param["pw"], PASSWORD_BCRYPT);
+                $this-> model-> insUser($param);
+                return "redirect:signin";
+        }
     }
 }
