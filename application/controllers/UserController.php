6fcb1cf9c888beeb514f62e99d3ebde1ee0cba7d
HYE0139 <h.ye011096@gamil.com>
2022-06-24 15:02:22 +0900

    first commit

---
+++ application/controllers/UserController.php
@@ -0,0 +1,15 @@
+<?php
+namespace application\controllers;
+
+require_once "application/utils/UrlUtils.php";
+
+class UserController extends Controller {
+    public function signin() {        
+        return "user/signin.php";
+    }
+
+    public function signup() {
+        return "user/signup.php";
+        
+    }
+}
