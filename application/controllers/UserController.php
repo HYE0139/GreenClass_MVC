94765f7ae6dd3ef03abedb47f1c912a96b9a5481
HYE0139 <h.ye011096@gmail.com>
2022-06-27 17:29:32 +0900

    로그인 강화

--- application/controllers/UserController.php
+++ application/controllers/UserController.php
@@ -10,13 +10,12 @@ class UserController extends Controller {
             case _POST : 
                 $email = $_POST["email"];
                 $pw = $_POST["pw"];
-                $param = ["email" => $email, "pw" => $pw ];
+                $param = ["email" => $email];
                 $dbUser = $this-> model-> selUser($param);
 
-            if(!$dbUser || !password_verify($pw, $dbUSer->pw)) {
+            if(!$dbUser || !password_verify($pw, $dbUser->pw)) {
                 return "redirect:signin?email={$email}&err";
             }
-
             $dbUser->pw = null; // 비밀번호 유출을 막기 위해 null값을 세션에 넣는다.
             $dbUser->regdt = null; // created_at ...메모리 사용을 줄이기 위해 필요없는 부분은 삭제
             $this->flash(_LOGINUSER, $dbUser); 
@@ -26,17 +25,27 @@ class UserController extends Controller {
 
     public function signup() {
         switch(getMethod()) {
-            case _GET : // 로그인
+            case _GET:
                 return "user/signup.php";
-            case _POST : //회원가입
+            case _POST:
+                $email = $_POST["email"];
+                $pw = $_POST["pw"];
+                $hashedPw = password_hash($pw, PASSWORD_BCRYPT);
+                $nm = $_POST["nm"];
                 $param = [
-                    "email" => $_POST["email"],
-                    "pw" => $_POST["pw"],
-                    "nm" => $_POST["nm"],
+                    "email" => $email,
+                    "pw" => $hashedPw,
+                    "nm" => $nm
                 ];
-                $param["pw"] = password_hash($param["pw"], PASSWORD_BCRYPT);
-                $this-> model-> insUser($param);
+
+                $this->model->insUser($param);
+
                 return "redirect:signin";
         }
+    }
+
+    public function logout() {
+        $this->flash(_LOGINUSER);
+        return "redirect:/user/signin";
     }
 }
