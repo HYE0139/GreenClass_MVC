069778bdcc1e90111bf0b06ee9138234543ee05f
HYE0139 <h.ye011096@gmail.com>
2022-06-27 17:31:40 +0900

    + JS

--- application/controllers/FeedController.php
+++ application/controllers/FeedController.php
@@ -3,7 +3,8 @@ namespace application\controllers;
 
 class FeedController extends Controller {
     public function index() {
-        $this -> addAttribute(_MAIN, "feed/index.php");
+        $this -> addAttribute(_MAIN, $this->getView("feed/index.php"));
+        $this -> addAttribute(_JS, ["feed/index"]);
         return "template/t1.php";
     }
 }
