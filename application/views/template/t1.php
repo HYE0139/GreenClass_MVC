0669040a31abd3c2e76813d66a279b066b296e4b
HYE0139 <h.ye011096@gmail.com>
2022-06-29 17:23:31 +0900

    레이아웃 수정

--- application/views/template/t1.php
+++ application/views/template/t1.php
@@ -3,10 +3,8 @@
 <?php include_once "application/views/template/head.php"; ?>
 
 <body>
-    <div class="container">
-        <header><?php include_once "application/views/template/header.php"; ?></header>
-        <main><?php include_once $this->main;?></main>
-        <footer><?php include_once "application/views/template/footer.php";?></footer>
-    </div>
+    <header><?php include_once "application/views/template/header.php"; ?></header>
+    <main><?php include_once $this->main;?></main>
+    <footer><?php include_once "application/views/template/footer.php";?></footer> 
 </body>
 </html>
