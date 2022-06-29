d7a7d813a04a63fa3c43983fef2fedf89004a2d0
HYE0139 <h.ye011096@gmail.com>
2022-06-29 17:23:57 +0900

    로그인 링크 추가

--- application/views/user/signup.php
+++ application/views/user/signup.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
 <?php include_once "application/views/template/head.php"; ?>
-<body>
+<body class="container-center">
     <div>
         <h1>회원가입</h1>
-
+        <a href="signin">로그인</a>
         <form action="signup" method="post">
             <div><input type="email" name="email" placeholder="email" autofocus required></div>
             <div><input type="password" name="pw" placeholder="password" required></div>
             <div><input type="txet" name="nm" placeholder="name" required></div>
             <div>
-                <input type="submit" value="회원가입">
+                <input type="submit" value="가입하기">
             </div>
         </form>
     </div>
