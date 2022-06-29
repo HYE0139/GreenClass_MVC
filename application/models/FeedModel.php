57fc2cea19ae669bd99d0d7ce591f3ca6a1db8bd
HYE0139 <h.ye011096@gmail.com>
2022-06-29 17:19:39 +0900

    DB feed 가져오기

--- application/models/FeedModel.php
+++ application/models/FeedModel.php
@@ -3,5 +3,58 @@
     use PDO;
 
     class FeedModel extends Model {
-        
+        public function insFeed(&$param) {
+            $sql = "INSERT INTO t_feed
+                    (location, ctnt, iuser)
+                    VALUES
+                    (:location, :ctnt, :iuser)
+            ";
+
+            $stmt= $this->pdo->prepare($sql);
+            $stmt -> execute(array($param["location"],$param["ctnt"],$param["iuser"]));
+            return intval($this->pdo->lastInsertId());
+        }
+
+        public function insFeedImg(&$param) {
+            $sql = "INSERT INTO t_feed_img
+                    (ifeed, img)
+                    VALUES
+                    (:ifeed, :feedImg)
+            ";
+
+            $stmt = $this->pdo->prepare($sql);
+            $stmt -> execute(array( $param["ifeed"], $param["feedImg"]));
+            
+        }
+
+        public function selFeedList(&$param) {
+            $sql = "SELECT A.ifeed, A.location, A.ctnt, A.iuser, A.regdt
+                         , C.nm AS writer, C.mainimg
+                         , IFNULL(E.cnt, 0) AS favCnt
+                         , IF(D.ifeed IS NULL, 0, 1) AS isFav
+                      FROM t_feed A
+                INNER JOIN t_user C 
+                        ON A.iuser = C.iuser
+                 LEFT JOIN
+                        (
+                            SELECT ifeed, COUNT(ifeed) AS cnt
+                              FROM t_feed_fav
+                          GROUP BY ifeed
+                        ) E
+                        ON A.ifeed = E.ifeed
+                 LEFT JOIN
+                        (
+                            SELECT ifeed, COUNT(ifeed) AS isFav
+                              FROM t_feed_fav
+                             WHERE iuser = :iuser
+                          GROUP BY ifeed
+                        ) D
+                        ON A.ifeed = D.ifeed
+                  ORDER BY A.ifeed DESC
+                     LIMIT :startIdx, :feedItemCnt;
+            ";
+            $stmt = $this ->pdo ->prepare($sql);
+            $stmt -> execute(array( $param["iuser"], $param["startIdx"], _FEED_ITEM_CNT));
+            return $stmt->fetchAll(PDO::FETCH_OBJ);
+        }
     }
