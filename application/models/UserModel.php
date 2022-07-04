f2c0f132f1dd83e13f05746f6c0db0e45d7c9730
HYE0139 <h.ye011096@gmail.com>
2022-07-04 21:41:46 +0900

    팔로어 작업 완료

--- application/models/UserModel.php
+++ application/models/UserModel.php
@@ -35,6 +35,9 @@ class UserModel extends Model {
 		              ,(SELECT COUNT(ifeed) FROM t_feed WHERE iuser = {$feediuser}) AS feedcnt
                       ,(SELECT COUNT(fromiuser) FROM t_user_follow WHERE fromiuser = {$feediuser} AND toiuser = {$loginiuser}) AS follower
 		              ,(SELECT COUNT(fromiuser) FROM t_user_follow WHERE fromiuser = {$loginiuser} AND toiuser = {$feediuser}) AS following
+                      ,(SELECT COUNT(toiuser) FROM t_user_follow WHERE toiuser = $feediuser) AS followerCnt
+                      ,(SELECT COUNT(toiuser) FROM t_user_follow WHERE fromiuser =$feediuser) AS followCnt
+                                            
                   FROM t_user
                  WHERE iuser = {$feediuser}";
         $stmt = $this->pdo->prepare($sql);
@@ -69,5 +72,38 @@ class UserModel extends Model {
         $stmt -> execute(array($param['fromiuser'], $param['toiuser']));
         return $stmt -> rowCount();
 
+    }
+    // ------------------------------Feed--------------------------------//
+    public function selFeedList(&$param) {
+        $iuser = $param["iuser"];
+        $sql = "SELECT A.ifeed, A.location, A.ctnt, A.iuser, A.regdt
+                    , C.nm AS writer, C.mainimg
+                    , IFNULL(E.cnt, 0) AS favCnt
+                    , IF(F.ifeed IS NULL, 0, 1) AS isFav
+                FROM t_feed A
+                INNER JOIN t_user C
+                ON A.iuser = C.iuser
+                AND C.iuser = {$iuser}
+                LEFT JOIN 
+                    (
+                        SELECT ifeed, COUNT(ifeed) AS cnt 
+                        FROM t_feed_fav
+                        GROUP BY ifeed
+                    ) E
+                ON A.ifeed = E.ifeed
+                LEFT JOIN
+                    (
+                        SELECT ifeed
+                        FROM t_feed_fav
+                        WHERE iuser = {$iuser}
+                    ) F
+                ON A.ifeed = F.ifeed
+                ORDER BY A.ifeed DESC
+                LIMIT :startIdx, :feedItemCnt";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->bindValue(":startIdx", $param["startIdx"]);
+        $stmt->bindValue(":feedItemCnt", _FEED_ITEM_CNT);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_OBJ);        
     }
 }
