57fa388a3bd4716c977204e28b753e8c85438196
HYE0139 <h.ye011096@gmail.com>
2022-07-05 18:30:06 +0900

    feed 수정

--- application/controllers/FeedController.php
+++ application/controllers/FeedController.php
@@ -5,7 +5,7 @@ use application\libs\Application;
 class FeedController extends Controller {
     public function index() {
         $this -> addAttribute(_MAIN, $this->getView("feed/index.php"));
-        $this -> addAttribute(_JS, ["feed/index", "feed/common_feed", "https://unpkg.com/swiper@8/swiper-bundle.min.js"]);
+        $this -> addAttribute(_JS, ["feed/index", "https://unpkg.com/swiper@8/swiper-bundle.min.js"]);
         $this -> addAttribute(_CSS, ["feed/index", "https://unpkg.com/swiper@8/swiper-bundle.min.css"]);
         return "template/t1.php";
     }
@@ -16,15 +16,15 @@ class FeedController extends Controller {
                 if(!is_array($_FILES) || !isset($_FILES["imgs"])) {
                     return ["result" => 0];
                 }
-                
+                $iuser = getIuser();//SessionUtils
                 $param = [
                     "location" => $_POST["location"],
                     "ctnt" => $_POST["ctnt"],
-                    "iuser" => getIuser()//SessionUtils
+                    "iuser" => $iuser
                 ];
 
                 $ifeed = $this-> model-> insFeed($param);
-                
+                $paramImg = [ "ifeed" => $ifeed ];
                 foreach($_FILES["imgs"]["name"] as $key => $originFileNm) {
                     // $file_name = explode(".", $value); =>  이미지 파일 이름을 '.' 기준 배열로 반환
                     // $ext = end($file_name);  => 배열의 마지막 = 확장자
@@ -34,13 +34,17 @@ class FeedController extends Controller {
                     }
                     
                     $tempName = $_FILES["imgs"]["tmp_name"][$key]; // FileUtils
-                    $randomImg = getRandomFileNm($originFileNm);
-                    if(move_uploaded_file($tempName, $saveDirectory. "/".$randomImg)) {
-                        $param = ["feedImg" => $randomImg, "ifeed" => $ifeed];
-                        $this-> model-> insFeedImg($param);
+                    $randomFileNm = getRandomFileNm($originFileNm);
+                    if(move_uploaded_file($tempName, $saveDirectory. "/".$randomFileNm)) {
+                        $paramImg["img"] = $randomFileNm;
+                        $this-> model-> insFeedImg($paramImg);
                     }
                 }
-                return [_RESULT => 1];
+
+                $param2 = [ "ifeed" => $ifeed ];
+                $data = $this->model->selFeedAfterReg($param2);
+                $data->imgList = $this->model->selFeedImgList($param2);
+                return $data;
 
             case _GET :
                 $page = 1;
@@ -54,8 +58,8 @@ class FeedController extends Controller {
                 ];
                 $list = $this->model->selFeedList($param);
                 foreach($list as $item) {
-                    $item->imgList = $this->model->selFeedImgList($item);
                     $param2 = ["ifeed" => $item->ifeed];
+                    $item->imgList = $this->model->selFeedImgList($param2);
                     $item->cmt = Application::getModel("feedcmt")->selFeedCmt($param2);
                 }
                 return $list;
