109d7f8cdf2918a70f7c152f13fe691de451eaf1
HYE0139 <h.ye011096@gmail.com>
2022-07-28 17:23:53 +0900

    쿼리스트링, Url 관련 메소드

---
+++ instagram_clone/application/utils/UrlUtils.php
@@ -0,0 +1,32 @@
+<?php
+    function getJson() {
+        return json_decode(file_get_contents('php://input'), true);
+        //json 데이터를 배열로 저장
+    }
+
+    function getParam($key) { // 쿼리스트링에 있었으면 GET방식으로 아니면 빈값으로
+        return isset($_GET[$key]) ? $_GET[$key] : "";
+    }
+    function getUrl() {
+        return isset($_GET['url']) ? rtrim($_GET['url'], '/') : "";
+    }
+    function getUrlPaths() {
+        $getUrl = getUrl(); // url을 '/'를 기준으로 나눠서 배열로 저장        
+        return $getUrl !== "" ? explode('/', $getUrl) : "";
+    }
+
+    function getMethod() {
+        return $_SERVER['REQUEST_METHOD'];
+        // $headers = getallheaders();
+        // return $headers['Accept'];
+    }
+
+    function isGetOne() {
+        $urlPaths = getUrlPaths();
+        if(isset($urlPaths[2])) { //one
+            return $urlPaths[2];
+        }
+        return false;
+    }
+
+  
